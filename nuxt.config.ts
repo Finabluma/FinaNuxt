6d58bc3bc08ab4dc5ea36cff23683358d6ff07f7
Finabluma <oscar.francino@finabluma.com>
2025-09-05 16:13:25 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  app:{
+    head:{
+      title: 'mi app',
+      titleTemplate: '%s | Webdevelopment'
+    }
+  },
+  $development:{
+    app:{
+      head:{
+        title:'DEV',
+        titleTemplate: '%s | DEV'
+      }
+    }
+  },
+  $production:{
+    app:{
+      head:{
+        title:'Prod',
+        titleTemplate: '%s | PROD'
+      }
+    }
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ]
+})
